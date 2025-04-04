983c994b986c8b8e5068b011f439cb7195145676
Jonah Silverman <jonahsilverman11@gmail.com>
2025-04-04 20:46:21 +0000

    init commit

---
+++ src/extract_barcodes.py
@@ -0,0 +1,324 @@
+import gzip
+import itertools
+import logging
+from collections import Counter, defaultdict
+from pathlib import Path
+import pickle
+import click
+import networkx as nx
+import numpy as np
+import scipy.io
+import scipy.sparse
+from matplotlib.backends.backend_pdf import PdfPages
+from sklearn.neighbors import radius_neighbors_graph
+
+import yaml
+
+import subprocess
+import os
+
+print("Begining Barcode Extraction")
+
+log = logging.getLogger("barcode_matrix")
+
+# get current file path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# set config yaml path as one directory above current file path
+config_dir = os.path.dirname(current_dir)
+config_path = os.path.join(config_dir, "config.yaml")
+with open(config_path, "r") as stream:
+    config = yaml.safe_load(stream)
+
+ug_exec_path = config["ugrep_path"]
+vt_start_index_r2 = config["vt_start_index_r2"]
+
+# WSBWSDWSHWSVWSBWSWSHWSVWSBWSDWSH
+DEGENERATE_BASE_DICT = {
+    "A": {"A"},
+    "C": {"C"},
+    "G": {"G"},
+    "T": {"T"},
+    "R": {"A", "G"},
+    "Y": {"C", "T"},
+    "M": {"A", "C"},
+    "K": {"G", "T"},
+    "S": {"C", "G"},
+    "W": {"A", "T"},
+    "H": {"A", "C", "T"},
+    "B": {"C", "G", "T"},
+    "V": {"A", "C", "G"},
+    "D": {"A", "G", "T"},
+    "N": {"A", "C", "G", "T"},
+}
+
+REVERSE_COMP_DICT = str.maketrans('ACGTN', 'TGCAN')
+
+def match_tags(
+    cell_ID, umis, barcode_codes, barcode_cutoff
+):
+    no_id_struct = 0
+    pass_all = 0
+
+    n_bases_bc = len(cell_ID[0])
+    
+    raw_umis_per_tag = defaultdict(set)
+    raw_reads_per_umi = defaultdict(Counter)
+
+    distribution_of_barcode_errors = {}
+    for tag, umi in zip(cell_ID, umis):
+
+        # Skip if Cell ID does not match structure
+        
+        num_matches = sum(b in s for b, s in zip(tag, barcode_codes))
+        num_errors = n_bases_bc - num_matches
+        current_num_w_error_count = distribution_of_barcode_errors.get(num_errors, 0)
+        current_num_w_error_count+=1
+        distribution_of_barcode_errors[num_errors] = current_num_w_error_count
+
+
+        if num_matches < barcode_cutoff:
+            no_id_struct += 1
+            continue
+
+        pass_all += 1
+
+        raw_umis_per_tag[tag].add(umi)
+        raw_reads_per_umi[tag][umi] += 1
+
+    log.debug(f"IDs w/o correct ID structure: {no_id_struct}")
+    log.debug(f"Passed all: {pass_all}")
+
+    raw_umis_per_tag = {
+        tag_seq: len(umi_seq) for tag_seq, umi_seq in raw_umis_per_tag.items()
+    }
+
+    return raw_umis_per_tag, raw_reads_per_umi, distribution_of_barcode_errors, pass_all
+
+
+def write_matrix(upb, beads, tags, output_file):
+    m = scipy.sparse.dok_matrix((len(beads), len(tags)), dtype=np.int32)
+    b2i = {b: i for i, b in enumerate(beads)}
+    t2j = {t: j for j, t in enumerate(tags)}
+
+    for b, bd in upb.items():
+        for t, v in bd.items():
+            m[b2i[b], t2j[t]] = v
+
+    m = m.tocsr()
+
+    with gzip.open(output_file, "wb") as out:
+        scipy.io.mmwrite(out, m)
+
+    return m
+
+def write_matrix_1d(upb, beads, output_file):
+    m = scipy.sparse.dok_matrix((len(beads)), dtype=np.int32)
+    b2i = {b: i for i, b in enumerate(beads)}
+    t2j = {t: j for j, t in enumerate(tags)}
+
+    for b, bd in upb.items():
+        for t, v in bd.items():
+            m[b2i[b], t2j[t]] = v
+
+    m = m.tocsr()
+
+    with gzip.open(output_file, "wb") as out:
+        scipy.io.mmwrite(out, m)
+
+    return m
+
+def reverse_h_set(h_barcodes):
+    base_d_rev = {0: "A", 1: "C", 2: "G", 3: "T", 4: "N"}
+
+    return ["".join([ base_d_rev[c] for c in h_list ]) for h_list in h_barcodes]
+
+
+# WSBWSDWSHWSVWSBWSDWSHWSVWSBWSDWSH
+@click.command()
+@click.argument("fastq-r1", type=click.Path(exists=True))
+@click.argument("fastq-r2", type=click.Path(exists=True))
+@click.option(
+    "--output-dir",
+    help="Path for output",
+    required=True,
+)
+@click.option(
+    "--tag-sequence",
+    default="WSBWSDWSHWSVWSBWSWSHWSVWSBWSDWSH",
+    help="Format for the tag sequence",
+)
+@click.option(
+    "--constant-sequence-pretag",
+    default="TGCCACCCGTAGATCTCTCGA",
+    help="Constant sequence that should precede the tag",
+)
+@click.option(
+    "--constant-sequence-posttag",
+    default="CGATACCGAGCGCTGCACCGG",
+    help="Constant sequence that should follow the tag",
+)
+@click.option(
+    "--tag-mismatch",
+    type=int,
+    default=3,
+    help="Number of mismatches to allow in tag",
+    show_default=True,
+)
+@click.option(
+    "--const-mismatch",
+    type=int,
+    default=3,
+    help="Number of mismatches to allow in tag",
+    show_default=True,
+)
+@click.option(
+    "--only_summary",
+    type=bool,
+    default=False,
+    show_default=True,
+)
+@click.option("--debug", is_flag=True, help="Turn on debug logging")
+def main(
+    fastq_r1,
+    fastq_r2,
+    output_dir,
+    tag_sequence,
+    constant_sequence_pretag,
+    constant_sequence_posttag,
+    tag_mismatch,
+    const_mismatch,
+    only_summary=False,
+    debug=False,
+):
+    """
+    This script generates some plots for mapping barcoded reads.
+
+    Reads sequences from FASTQ_R1 and FASTQ_R2. Assumes that the first read
+    contains a 15bp barcode split across two locations, along with an 8bp UMI.
+    The second read is assumed to have TAG_SEQUENCE in bases 20-40.
+    """
+    # create_logger(debug, dryrun=False)
+
+    print(len(tag_sequence))
+    print(tag_sequence)
+    
+    # make output directory recursively
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_dir = Path(output_dir)
+    print(f"Saving output to {output_dir}")
+
+    command = [f'zcat {fastq_r1} | head -n 2 | tail -n 1 | wc -c']
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    rd1_length = result.stdout.strip()
+
+    rd1_length=int(rd1_length)
+    a=rd1_length-1
+    print(f'Read 1 is {a} nt long')
+
+    preform_processing = not only_summary
+    print(preform_processing)
+
+    # if rd2_stripped exists, skip this step
+    if (not (output_dir / 'rd2_stripped.fastq').exists()) & preform_processing:
+        print("Saving only the sequence lines from read 1")
+        command = [f'zcat {fastq_r1} | awk \'NR%4==2\' > rd1_stripped.fastq']
+        result = subprocess.run(command, cwd=output_dir, shell=True, capture_output=True)
+    else:
+        print("R1 already strippined. Skipping")
+    
+    # Check number of lines total
+    cmd = "zcat rd1_stripped.fastq | wc -l"
+    n_lines_total = subprocess.run(cmd, cwd=output_dir, shell=True, capture_output=True)
+    
+    if (not (output_dir / 'merged.fastq.gz').exists()) & preform_processing:
+        print("Saving only the sequence lines from read 2")
+        command = [f'zcat {fastq_r2} | awk \'NR%4==2\' > rd2_stripped.fastq']
+        result = subprocess.run(command, cwd=output_dir, shell=True, capture_output=True)
+    else:
+        print("R2 already strippined. Skipping")
+    if (not (output_dir / 'merged_preFilt_postFilt.fastq.gz').exists()) & preform_processing:
+        print("Merging Read 1 and Read 2 files")
+        command = [f'paste rd1_stripped.fastq rd2_stripped.fastq | gzip > merged.fastq.gz']
+        result = subprocess.run(command, cwd=output_dir, shell=True, capture_output=True)
+    else:
+        print("R1 and R2 Merged already strippined. Skipping")
+
+    if (not (output_dir / 'ident_preFilt_postFilt.fastq.gz').exists()) &  preform_processing:
+        print("Fuzzy grep-ing for polyA constant sequence")
+        print(constant_sequence_pretag)
+        command = [f'zcat merged.fastq.gz | {ug_exec_path} -Z3 {constant_sequence_pretag} |  {ug_exec_path} -Z3 {constant_sequence_posttag} | gzip > merged_preFilt_postFilt.fastq.gz']
+        result = subprocess.run(command, cwd=output_dir, shell=True, capture_output=True, text=True)
+
+    bc_size = len(tag_sequence)
+    
+    bc_start = rd1_length + vt_start_index_r2
+    bc_end = bc_start+bc_size-1
+  
+    print("Cutting out the identifiers/UMIs")
+    print("Cutting out the identifiers/UMIs")
+
+    if preform_processing:
+        command = [f'zcat merged_preFilt_postFilt.fastq.gz | cut -c 1-10 | gzip > umi_preFilt_postFilt.fastq.gz; zcat merged_preFilt_postFilt.fastq.gz | cut -c {bc_start}-{bc_end} | gzip > ident_preFilt_postFilt.fastq.gz']
+        result = subprocess.run(command, cwd=output_dir, shell=True, capture_output=True, text=True)
+
+    print(f"Reading identifiers/UMIs")
+    cellID_path = output_dir / 'ident_preFilt_postFilt.fastq.gz'
+    umi_path = output_dir / 'umi_preFilt_postFilt.fastq.gz'
+    with gzip.open(cellID_path, "rt") as fh:
+        cell_ID = [line.strip() for line in itertools.islice(fh, 0, None, 1)]
+    with gzip.open(umi_path, "rt") as fh:
+        umis = [line.strip() for line in itertools.islice(fh, 0, None, 1)]
+
+    assert len(cell_ID) == len(umis), "read different number of reads"
+    num_reads_post_const_grep = len(cell_ID)
+    
+    print(f"Total of {len(cell_ID)} reads")
+
+    barcode_codes = [DEGENERATE_BASE_DICT[b] for b in tag_sequence]
+    barcode_cutoff = len(tag_sequence) - tag_mismatch
+
+    print(f"barcode_cutoff: {barcode_cutoff}")
+
+    raw_umis_per_tag, raw_reads_per_umi, distribution_of_barcode_errors, num_reads_passing = match_tags(
+        cell_ID, umis, barcode_codes, barcode_cutoff
+    )
+
+    print("Writing output files")
+    raw_tags = sorted(raw_umis_per_tag.keys())
+    umis_count = [str(raw_umis_per_tag[t]) for t in raw_tags]
+    raw_umis = sorted({u for t in raw_tags for u in raw_reads_per_umi[t]})
+
+    print(f"{len(raw_tags)} unique bead IDs")
+
+    with gzip.open(output_dir / "raw_tags.txt.gz", "wt") as out:
+        print("\n".join(raw_tags), file=out)
+    with gzip.open(output_dir / "umis_per_tags.txt.gz", "wt") as out:
+        print("\n".join(umis_count), file=out)
+    with gzip.open(output_dir / "raw_umis.txt.gz", "wt") as out:
+        print("\n".join(raw_umis), file=out)
+
+    log.debug("Writing raw umi/reads matrix")
+    write_matrix(raw_reads_per_umi, raw_tags, raw_umis, output_dir / "raw_read_umi_matrix.mtx.gz")
+
+    print("saving filtering metadata")
+    filter_results = {
+        "raw_nreads": n_lines_total,
+        "post_const_grep_nreads": num_reads_post_const_grep,
+        "post_barcode_structure_nreads": num_reads_passing,
+        "distribution_matches_struc_errors": distribution_of_barcode_errors
+    }
+
+    meta_out_path = os.path.join(output_dir, "bc_extraction_meta.pkl")
+    pickle.dump(filter_results, open(meta_out_path, "wb"))
+
+    # remove intermediate files
+    print("Removing intermediate files")
+    command = [f'rm rd1_stripped.fastq rd2_stripped.fastq']
+    result = subprocess.run(command, cwd=output_dir, shell=True, capture_output=True)
+    
+    print("Done!")
+    
+if __name__ == "__main__":
+    main()
+
---
+++ src/graph_dedup_from_bcs.py
@@ -0,0 +1,259 @@
+import os
+import argparse
+import pickle
+import gzip
+import tqdm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import scipy.io
+import multiprocessing as mp
+import itertools
+from collections import Counter, defaultdict
+import sys
+
+
+#### Fill path to SynapseSeqTools ####
+# Ensure can import
+import synapse_seq_functions as ssf
+
+
+# Fill in the path to umicollapse
+# ex.) /opt/conda/share/umicollapse-1.0.0-1/umicollapse
+umicollapse_path="umicollapse"
+
+assert os.path.exists(umicollapse_path)
+
+def write_mock_fastq(group_name, umi_vts, size_umi, size_vt, mock_fastq_folder):
+    current_fastq_path = os.path.join(mock_fastq_folder, f"{group_name}.fastq")
+    # create mock fastq file
+    read_size = size_umi + size_vt
+    # Write mock quality string for formatting, but is not taken into account for algo
+    quality_string = "j" * read_size
+    with open(current_fastq_path, "w") as fh:
+        for umi_vt in umi_vts:
+            umi = umi_vt["umi"]
+            vt = umi_vt["vt"]
+            count = umi_vt["count"]
+            umi_vt = umi + vt
+            for i in range(count):
+                fh.write(f"@{group_name}\n")
+                fh.write(f"{umi_vt}\n")
+                fh.write(f"+\n")
+                fh.write(f"{quality_string}\n")
+    return current_fastq_path
+
+def run_umi_vt_collapse(group_name, mock_fastq_folder, output_dir, log_base = "/broad/macosko/logs"):
+    mock_fastq_in = os.path.join(mock_fastq_folder, f"{group_name}.fastq")
+    assert os.path.exists(mock_fastq_in)
+    mock_fastq_out = os.path.join(output_dir, f"{group_name}_out.fastq")
+    print(mock_fastq_out)
+    log_file = os.path.join(log_base, f"{group_name}_dedup_graph.log")
+    cmd = f"{umicollapse_path} -k 1 -i {mock_fastq_in} -o {mock_fastq_out} --tag > {log_file} 2>&1"
+    print(cmd)
+    os.system(cmd)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--base_dir", help="", required=True)
+parser.add_argument("-o", "--out_dir_general", help="", required=True)
+parser.add_argument("-r", "--run_id", help="", required=True)
+parser.add_argument("-t", "--is_test", action="store_true", required=False, default=False)
+args = parser.parse_args()
+
+base_dir = args.base_dir
+out_dir_general = args.out_dir_general
+run_id = args.run_id
+is_test = args.is_test
+
+region_in_path = os.path.join(base_dir, run_id)
+print(f"Checking that {region_in_path} exists")
+assert os.path.exists(region_in_path), f"File {region_in_path} does not exist"
+
+# Create output dir if it doesn't exist already
+out_dir = os.path.join(out_dir_general, run_id, "dedup")
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+print(f"Working out_dir: {out_dir}")
+
+intermediate_dir = os.path.join(out_dir,"intermediate")
+if not os.path.exists(intermediate_dir):
+    os.mkdir(intermediate_dir)
+
+# Create intermediate dir if it doesn't exist already. Used to store computed files
+intermediate_dir_path = os.path.join(out_dir, "intermediate")
+if not os.path.exists(intermediate_dir_path):
+    os.makedirs(intermediate_dir_path)
+
+sys.stderr.flush()
+sys.stdout.flush()
+
+# Hard coded local file names
+reads_per_umi_fname = "raw_read_umi_matrix.mtx.gz"
+vt_listfname = "raw_tags.txt.gz"
+umi_listfname = "raw_umis.txt.gz"
+
+
+# Read in umi-vt matrix to be umixvt in csr form
+reads_per_umi_fname_full = os.path.join(region_in_path, reads_per_umi_fname)
+print(f"Reading {reads_per_umi_fname_full}")
+reads_per_umi_mat = scipy.io.mmread(reads_per_umi_fname_full)
+reads_per_umi_mat = reads_per_umi_mat.tocsr()
+umi_vt_mat = reads_per_umi_mat.T.tocsr()
+print(reads_per_umi_mat.shape)
+
+# Read in list of VTs and UMIs
+vt_listfname_full = os.path.join(region_in_path, vt_listfname)
+print(f"Reading {vt_listfname_full}")
+sys.stderr.flush()
+sys.stdout.flush()
+
+vts = []
+with gzip.open(vt_listfname_full, "rt") as f:
+    for line in f:
+        vts.append(line.strip())
+
+umi_listfname_full = os.path.join(region_in_path, umi_listfname)
+print(f"Reading {umi_listfname_full}")
+umis = []
+with gzip.open(umi_listfname_full, "rt") as f:
+    for line in f:
+        umis.append(line.strip())
+
+bulk_obj = {"umi_vt_mat": umi_vt_mat, "umis": umis, "vts": vts}
+
+sys.stderr.flush()
+sys.stdout.flush()
+
+
+# If test subset matrix to first 1000 umi and vts
+if is_test:
+    print("IS TEST. SUbsetting UMI and VTs")
+    bulk_obj["umi_vt_mat"] = bulk_obj["umi_vt_mat"][:1000, :1000]
+    bulk_obj["umis"] = bulk_obj["umis"][:1000]
+    bulk_obj["vts"] = bulk_obj["vts"][:1000]
+
+# Create mock fastq and output dedup folders
+mock_fastq_folder = os.path.join(intermediate_dir, "mock_fastq")
+if not os.path.exists(mock_fastq_folder):
+    os.makedirs(mock_fastq_folder)
+
+# check if mock fastq exists
+mock_fastq_out_path = os.path.join(mock_fastq_folder, f"{run_id}.fastq")
+mock_fastq_exists = os.path.exists(mock_fastq_out_path)
+
+umi_dedup_outdir = os.path.join(intermediate_dir, "umi_dedup_fastqs")
+dedup_fastq_out_path = os.path.join(umi_dedup_outdir, f"{run_id}_out.fastq")
+dedup_fastq_exists = os.path.exists(dedup_fastq_out_path)
+
+print(f"Mock Fastq Exists: {mock_fastq_exists}")
+print(f"dedup Fastq Exists: {dedup_fastq_exists}")
+
+if not os.path.exists(umi_dedup_outdir):
+    os.makedirs(umi_dedup_outdir)
+
+# save size of umi and vt
+umi_len = len(bulk_obj["umis"][0])
+vt_len = len(bulk_obj["vts"][0])
+
+print(f"umi_len: {umi_len}")
+print(f"vt_len: {vt_len}")
+
+
+if not mock_fastq_exists:
+    print("creating bulk fastq list")
+    sys.stderr.flush()
+    sys.stdout.flush()
+    
+    # Running hamming correction on combo of umi and corrected vt
+    # Create list of umi-vt pairs and # reads to write to a mock fastq
+    bulk_fastq_list = []
+    n_rows_umi_mat = bulk_obj["umi_vt_mat"].shape[0]
+    for i in range(n_rows_umi_mat):
+        umi = bulk_obj["umis"][i]
+        nonzero_elements = bulk_obj["umi_vt_mat"][i, :].nonzero()[1]
+        for nonzero_element in nonzero_elements:
+            vt = bulk_obj["vts"][nonzero_element]
+            read_count = bulk_obj["umi_vt_mat"][i, nonzero_element]
+            umi_vt = {"umi": umi, "vt": vt, "count": read_count}
+            bulk_fastq_list.append(umi_vt)
+    # Create mock fastqs for umicollapse in parallel
+    print("Writing mock fastq")
+    write_mock_fastq(run_id, bulk_fastq_list, umi_len,vt_len, mock_fastq_folder)
+else:
+    print("Using already existing mock fastq")
+
+sys.stderr.flush()
+sys.stdout.flush()
+
+
+if not dedup_fastq_exists:
+    # Run umicollapse
+    print("Running umicollapse")
+    run_umi_vt_collapse(run_id, mock_fastq_folder, umi_dedup_outdir, log_base=out_dir)
+else:
+    print(f"Using already created dedup fastq at {dedup_fastq_out_path}")
+
+sys.stderr.flush()
+sys.stdout.flush()
+
+print("Parsing umicollapse results")
+bulk_umi_dedup_outs = ssf.parse_umi_collapse_results(run_id, umi_dedup_outdir)
+
+u_mat_dedup_vt_umi_data_dict = defaultdict(Counter)
+for i in tqdm.tqdm(range(bulk_obj["umi_vt_mat"].shape[0])):
+    current_umi = bulk_obj["umis"][i]
+
+    nonzero_vals = bulk_obj["umi_vt_mat"][i, :].nonzero()[1]
+    for nonzero_val in nonzero_vals:
+        vt = bulk_obj["vts"][nonzero_val]
+        count = bulk_obj["umi_vt_mat"][i, nonzero_val]
+
+        umi_vt = current_umi + vt
+
+        umi_vt_correction = bulk_umi_dedup_outs.get(umi_vt, None)
+        if umi_vt_correction is None:
+            print(f"None for {umi_vt}")
+        assert umi_vt_correction is not None
+        umi_corrected = umi_vt_correction[:umi_len]
+        vt_corrected = umi_vt_correction[umi_len:]
+
+        u_mat_dedup_vt_umi_data_dict[umi_corrected][vt_corrected] += count
+
+
+sys.stderr.flush()
+sys.stdout.flush()
+
+# Create a new u_mat with the corrected UMI sequences
+umis_from_dict = sorted(list(u_mat_dedup_vt_umi_data_dict.keys()))
+vts_from_dict = sorted(set(i for v in u_mat_dedup_vt_umi_data_dict.values() for i in v.keys()))
+u_mat_umi_dedup = ssf.create_matrix(u_mat_dedup_vt_umi_data_dict, umis_from_dict, vts_from_dict)
+u_mat_vt_and_umi_dedup_obj = {"u": u_mat_umi_dedup, "umis": umis_from_dict, "vts": vts_from_dict}
+
+
+
+u_mat_non_max_out = os.path.join(out_dir, "u_mat_dedup.pkl")
+print(f"Writing u_mat to {u_mat_non_max_out}")
+with open(u_mat_non_max_out, "wb") as fh:
+    pickle.dump(u_mat_vt_and_umi_dedup_obj, fh)
+
+# For each umi only keep reads for max vt
+u_mat_dedup_max = ssf.filter_to_max_vt_per_cbumi(u_mat_vt_and_umi_dedup_obj["u"], u_mat_vt_and_umi_dedup_obj["umis"], u_mat_vt_and_umi_dedup_obj["vts"])
+u_mat_dedup_max_obj = {"u": u_mat_dedup_max, "umis": u_mat_vt_and_umi_dedup_obj["umis"], "vts": u_mat_vt_and_umi_dedup_obj["vts"]}
+
+# Save pre read filt object
+u_mat_out = os.path.join(out_dir, "u_mat_dedup_max.pkl")
+print(f"Writing u_mat to {u_mat_out}")
+with open(u_mat_out, "wb") as fh:
+    pickle.dump(u_mat_dedup_max_obj, fh)
+
+
+# remove the mock fastq created and gzip the deduped fastqs
+# print("removing mock fastq folder")
+# os.system(f"rm -r {mock_fastq_folder}")
+# print("gzipping deduped fastqs")
+# os.system(f"find {umi_dedup_outdir} -type f -exec gzip {{}} \\;")
+
+print("Finished")
+
